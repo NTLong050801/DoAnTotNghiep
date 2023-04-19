979be9ece47846e5ea76ed64ab0dafa9160f34d8
nguyen thanh long <loglog050801>
2023-04-19 20:14:17 +0700

    update bảng user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,15 +12,29 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::create('majors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->nullable();
+            $table->string('identifier')->unique()->nullable();
+            $table->foreignId('major_id')->nullable()->constrained('majors');
             $table->rememberToken();
             $table->timestamps();
         });
+        \App\Models\User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin'),
+            'role' => '2',
+        ]);
     }
 
     /**
@@ -27,6 +42,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('majors');
         Schema::dropIfExists('users');
     }
 };
