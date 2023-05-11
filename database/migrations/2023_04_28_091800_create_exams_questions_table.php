05872606526691bb408ed513c01ab0c835dbf07f
nguyen thanh long <loglog050801>
2023-05-11 21:58:56 +0700

    tạo migartion classes

--- database/migrations/2023_04_28_091800_create_exams_questions_table.php
+++ database/migrations/2023_04_28_091800_create_exams_questions_table.php
@@ -13,8 +13,21 @@ return new class extends Migration
     {
         Schema::create('exams_questions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('exam_id')->constrained('exams');
-            $table->foreignId('question_id')->constrained('questions');
+            $table->foreignId('exam_id')->constrained('exams')->cascadeOnDelete();
+            $table->foreignId('question_id')->constrained('questions')->cascadeOnDelete();
+            $table->timestamps();
+        });
+
+        Schema::create('exams_students', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('exam_id')->constrained('exams')->cascadeOnDelete();
+            $table->foreignId('id_user')->constrained('users')->cascadeOnDelete();
+            $table->date('time_started')->nullable();
+            $table->longText('questions')->nullable();
+            $table->longText('answers')->nullable();
+            $table->float('result')->nullable();
+            $table->text('warning')->nullable();
+            $table->boolean('isActive')->default(false);
             $table->timestamps();
         });
     }
