e1fcb91e611812ae010580bacaad4820ff81c43f
nguyen thanh long <loglog050801>
2023-07-17 23:04:17 +0700

    fix oder by

--- app/Http/Controllers/Teachers/ResultsController.php
+++ app/Http/Controllers/Teachers/ResultsController.php
@@ -20,10 +20,10 @@ class ResultsController extends Controller
     }
 
     public function index($exam_id = null){
-        $exams = Exam::where('user_id',auth()->id())->where('is_end',true)->get();
+        $exams = Exam::where('user_id',auth()->id())->where('is_end',true)->orderBy('id','desc')->get();
         $examsStudents = null;
         if ($exam_id != null){
-            $examsStudents = ExamsStudents::where('exam_id',$exam_id)->get();
+            $examsStudents = ExamsStudents::where('exam_id',$exam_id)->orderBy('id','desc')->get();
         }
         return view('pages.teachers.results.index',compact('exams','examsStudents'));
     }
